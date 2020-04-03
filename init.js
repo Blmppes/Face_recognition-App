f91182eef3b4e0437830601e9f2c6b8184417bb8
HioNobi <43724804+HioNobi@users.noreply.github.com>
2020-04-03 19:06:11 +0700

    Updated

--- init.js
+++ init.js
@@ -1,8 +1,15 @@
-firebase.initializeApp({
-    apiKey: 'AIzaSyAlieG28M5AmvPAcNpG7L5uXB_MYirZhvE',
-    authDomain: 'https://test 123.firebaseapp.com',
-    projectId: 'test-123-256509'
-});
+const firebaseConfig = {
+    apiKey: "AIzaSyAlieG28M5AmvPAcNpG7L5uXB_MYirZhvE",
+    authDomain: "test-123-256509.firebaseapp.com",
+    databaseURL: "https://test-123-256509.firebaseio.com",
+    projectId: "test-123-256509",
+    storageBucket: "test-123-256509.appspot.com",
+    messagingSenderId: "230215355272",
+    appId: "1:230215355272:web:271668e17710f23674c83d",
+    measurementId: "G-FMZFNQ0T6Q"
+  };
+
+firebase.initializeApp(firebaseConfig);
   
 const db = firebase.firestore();
 const auth = firebase.auth();
